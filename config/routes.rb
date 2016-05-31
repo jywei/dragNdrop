24ada0d9c0d8601e56a83d679c865955bbde6e10
Roy Wei <roywjy@gmail.com>
2016-05-31 17:23:35 +0800

    add routes, js, index.html, media_contents_controller, media_uploader, medium.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :media_contents, only: [:create]
+
+  root 'media_contents#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
